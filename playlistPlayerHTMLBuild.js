19e6640c55931d9dfea5e2e82b861478bb0b3b4b
sweeneya <sweeneya@dor.state.ma.us>
2018-08-20 18:15:53 -0400

    Add files via upload

--- playlistPlayerHTMLBuild.js
+++ playlistPlayerHTMLBuild.js
@@ -1,3 +1,10 @@
+/*YouTube Playlist Player   written by Iris Wang and Alan Sweeney under direction of Peter Olejnik Copyrighted 2018 Version 1.0 Sept 1, 2018.  You can redistribute and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed. 
+*/
+
 // API key to use the YouTube API
 var apiKey = "AIzaSyDbwI3YwWxbg105jMVDcKOWl348UKHmIMw";
 //var apiKey = "AIzaSyDag4I45HFd7eS92OQYOZa-71ftsJRZoIo";
